5fecff4ace1d95f33f819780734e2b468d7e0b1d
andre souza <andssouzza@gmail.com>
2024-10-09 11:50:28 -0400

    chore: add migrations

---
+++ prisma/migrations/20241009004040_add_a_column_image_url/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Hero" ADD COLUMN     "image_url" TEXT;
---
+++ prisma/migrations/20241009010700_remove_column_image_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image_url` on the `Hero` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Hero" DROP COLUMN "image_url";
